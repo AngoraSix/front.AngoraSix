b85031ef507fc974a29f5f076476e0817f860046
Ger Roza <rozagerardo@gmail.com>
2022-12-24 19:40:04 -0300

    Added support for Management Bylaws

--- components/Project/Management/View/Sections/ManagementCoreSection/ManagementCoreSection.component.js
+++ components/Project/Management/View/Sections/ManagementCoreSection/ManagementCoreSection.component.js
@@ -1,10 +1,24 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { useTheme } from '@mui/styles';
 import { useTranslation } from 'next-i18next';
 import PropTypes from 'prop-types';
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+const translateOrValue = (t, i18n, i18nKey, value) => {
+  console.log('GERRRRR');
+  console.log(capitalizeFirstLetter(value));
+  return i18n.exists(i18nKey, {
+    ns: 'project-management.view',
+  })
+    ? t(i18nKey)
+    : capitalizeFirstLetter(value);
+};
+
 const ManagementCoreSection = ({ project, projectManagement }) => {
-  const { t } = useTranslation('project-management.view');
+  const { t, i18n } = useTranslation('project-management.view');
   const theme = useTheme();
 
   return (
@@ -14,22 +28,56 @@ const ManagementCoreSection = ({ project, projectManagement }) => {
         backgroundColor: theme.palette.primary.light,
       }}
     >
-      <Box className="ManagementCoreSection__Field">
-        <Box className="ManagementCoreSection__Field__Title">
-          <Typography align="center">
-            {t('project-management.view.status')}
-          </Typography>
+      <Box className="ManagementCoreSection__Status">
+        <Box className="ManagementCoreSection__Field">
+          <Box className="ManagementCoreSection__Field__Title">
+            <Typography align="center">
+              {t('project-management.view.status')}
+            </Typography>
+          </Box>
+          <Box className="ManagementCoreSection__Field__Value">
+            <Typography
+              align="center"
+              variant="h6"
+              component="h4"
+              color="primary.contrastText"
+            >
+              {t(`project-management.view.status.${projectManagement.status}`)}
+            </Typography>
+          </Box>
         </Box>
-        <Box className="ManagementCoreSection__Field__Value">
-          <Typography
-            align="center"
-            variant="h6"
-            component="h4"
-            color="primary.contrastText"
-          >
-            {t(`project-management.view.status.${projectManagement.status}`)}
-          </Typography>
-        </Box>
+      </Box>
+      <Box className="ManagementCoreSection__Bylaws">
+        <Grid container spacing={2}>
+          {projectManagement.constitution.bylaws.map((bylaw) => {
+            return (
+              <Grid item xs={6} key={bylaw.scope}>
+                <Box className="ManagementCoreSection__Field">
+                  <Box className="ManagementCoreSection__Field__Title">
+                    <Typography align="center">
+                      {translateOrValue(
+                        t,
+                        i18n,
+                        `project-management.view.bylaws.${bylaw.scope}`,
+                        bylaw.scope
+                      )}
+                    </Typography>
+                  </Box>
+                  <Box className="ManagementCoreSection__Field__Value">
+                    <Typography align="center" color="primary.contrastText">
+                      {translateOrValue(
+                        t,
+                        i18n,
+                        `project-management.view.bylaws.${bylaw.scope}.${bylaw.definition}`,
+                        bylaw.definition
+                      )}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Grid>
+            );
+          })}
+        </Grid>
       </Box>
     </Box>
   );
