8e1947c1dbaecc233d65692e568dbb37c6d3bfbd
Ger Roza <rozagerardo@gmail.com>
2025-04-07 23:45:59 -0300

    Defining Tasks stats query params from 'params' object, normalized

--- api/managementTasks.js
+++ api/managementTasks.js
@@ -1,14 +1,26 @@
 import config from '../config';
 import { obtainInfraHeaders } from '../utils/infra';
 
+const SUPPORTED_PARAMS = {
+  recentPeriodDays: 'recentPeriodDays',
+  sortField: 'sortField',
+};
+
+const _normalizeParams = (params, allowedKeys = Object.values(SUPPORTED_PARAMS)) => {
+  // Ensure params is an object. If not, default to an empty object.
+  return Object.fromEntries(
+    Object.entries(params || {}).filter(
+      ([key, value]) => allowedKeys.includes(key) && value !== undefined && value !== null
+    )
+  );
+}
+
 class ProjectManagementTasksAPI {
   constructor(axiosInstance) {
     this.axios = axiosInstance;
   }
 
-  async resolveProjectManagementTasks(projectManagementId, token) {
-    const recentPeriodDays = 1;
-    const sortField = 'recentlyCompletedEffort';
+  async resolveProjectManagementTasks(projectManagementId, token, params) {
 
     const headers = this.axios.getCommonHeaders();
     const authHeaders = this.axios.getAuthorizationHeaders(token, false);
@@ -17,14 +29,17 @@ class ProjectManagementTasksAPI {
       config.api.serverBaseURL
     );
 
+    const normalizedParams = _normalizeParams(params);
+
     const { data } = await this.axios.get(
-      `/project-management/${projectManagementId}/stats?recentPeriodDays=${recentPeriodDays}&sortField=${sortField}`,
+      `/project-management/${projectManagementId}/stats`,
       {
         headers: {
           ...headers,
           ...authHeaders,
           ...infraHeaders,
         },
+        params: normalizedParams
       }
     );
     return data;
