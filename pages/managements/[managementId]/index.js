8588981c15c39e26833b5934752b609dd545a3e7
Ger Roza <rozagerardo@gmail.com>
2025-11-08 23:29:47 -0300

    Fixed small bugs and reformatted

--- pages/managements/[managementId]/index.js
+++ pages/managements/[managementId]/index.js
@@ -1,15 +1,15 @@
-import { Box } from '@mui/material';
-import { getSession } from 'next-auth/react';
-import { useTranslation } from 'next-i18next';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import PropTypes from 'prop-types';
-import api from '../../../api';
-import FormSkeleton from '../../../components/common/Skeletons/FormSkeleton.component';
-import ProjectManagementView from '../../../components/Management/View';
-import ManagementDetailsLayout from '../../../layouts/ManagementDetailsLayout';
-import { obtainValidatedToken } from '../../../utils/api/apiHelper';
-import { isA6ResourceAdmin } from '../../../utils/commons/a6commonsUtils';
-import logger from '../../../utils/logger';
+import { Box } from '@mui/material'
+import { getSession } from 'next-auth/react'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import PropTypes from 'prop-types'
+import api from '../../../api'
+import FormSkeleton from '../../../components/common/Skeletons/FormSkeleton.component'
+import ProjectManagementView from '../../../components/Management/View'
+import ManagementDetailsLayout from '../../../layouts/ManagementDetailsLayout'
+import { obtainValidatedToken } from '../../../utils/api/apiHelper'
+import { isA6ResourceAdmin } from '../../../utils/commons/a6commonsUtils'
+import logger from '../../../utils/logger'
 
 const ProjectManagementViewPage = ({
   project,
@@ -21,10 +21,10 @@ const ProjectManagementViewPage = ({
   isAdmin,
   session,
 }) => {
-  const { t } = useTranslation('management.view');
+  const { t } = useTranslation('management.view')
 
   if (!projectManagement) {
-    logger.error('Log in to see management dashboard');
+    logger.error('Log in to see management dashboard')
     return (
       <ManagementDetailsLayout
         headData={{
@@ -37,7 +37,7 @@ const ProjectManagementViewPage = ({
           <FormSkeleton />
         </Box>
       </ManagementDetailsLayout>
-    );
+    )
   }
 
   return (
@@ -65,48 +65,54 @@ const ProjectManagementViewPage = ({
         isAdmin={isAdmin}
       />
     </ManagementDetailsLayout>
-  );
-};
+  )
+}
 
 ProjectManagementViewPage.defaultProps = {
   projectManagementActions: {},
-};
+}
 
 ProjectManagementViewPage.propTypes = {
   project: PropTypes.object.isRequired,
   projectManagement: PropTypes.object.isRequired,
   projectManagementActions: PropTypes.object,
   isAdmin: PropTypes.bool,
-};
+}
 
 export const getServerSideProps = async (ctx) => {
-  let props = { isAdmin: false };
-  const { managementId } = ctx.params;
-  const session = await getSession(ctx);
-  const validatedToken = await obtainValidatedToken(ctx.req);
+  let props = { isAdmin: false }
+  const { managementId } = ctx.params
+  const session = await getSession(ctx)
+  const validatedToken = await obtainValidatedToken(ctx.req)
 
   try {
     const projectManagement = await api.management.getProjectManagement(
       managementId,
       validatedToken
-    );
+    )
 
-    const project = projectManagement.project;
-    const projectManagementActions = {};
+    const project = projectManagement.project
+    const projectManagementActions = {}
 
     const projectManagementTasksStats =
       await api.managementTasks.resolveProjectManagementTasks(
         managementId,
         validatedToken
-      );
+      )
 
     const projectManagementAccountingStats =
       await api.managementAccounting.resolveProjectManagementAccounting(
         managementId,
         validatedToken
-      );
-    const projectContributorIds = projectManagementTasksStats.project.contributors.map(c => c.contributorId);
-    const contributorsData = await api.contributors.listContributors(projectContributorIds, validatedToken);
+      )
+    const projectContributorIds =
+      projectManagementTasksStats.project.contributors.map(
+        (c) => c.contributorId
+      )
+    const contributorsData = await api.contributors.listContributors(
+      projectContributorIds,
+      validatedToken
+    )
     props = {
       ...props,
       project,
@@ -116,9 +122,9 @@ export const getServerSideProps = async (ctx) => {
       contributorsData,
       projectManagementActions,
       isAdmin: isA6ResourceAdmin(session?.user?.id, projectManagement),
-    };
+    }
   } catch (err) {
-    logger.error('err', err);
+    logger.error('err', err)
   }
   return {
     props: {
@@ -131,7 +137,7 @@ export const getServerSideProps = async (ctx) => {
         'management.view',
       ])),
     },
-  };
-};
+  }
+}
 
-export default ProjectManagementViewPage;
+export default ProjectManagementViewPage
